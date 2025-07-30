3d47c0b90e7854f43a7d7ae389f70b59c4ffec12
IsmaelEFC <132825318+IsmaelEFC@users.noreply.github.com>
2025-07-30 14:32:06 -0400

    Delete scripts directory

--- scripts/redirect.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('consultar').addEventListener('click', () => {
-    const ppu = document.getElementById('ppu-result').textContent;
-    if (ppu && ppu !== '----' && ppu !== 'No detectado') {
-      window.open(`https://www.autoseguro.gob.cl/?placa=${encodeURIComponent(ppu)}`, '_blank');
-    } else {
-      alert('Primero debes capturar una patente válida.');
-    }
-  });
-  
